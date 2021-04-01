190ee9ada93355b4ec0eba100d74c95fd811cb26
유하람 <w2036@e-mirim.hs.kr>
2021-04-01 20:12:43 +0900

    직접풀어보기 4_2 연습

---
+++ src/main/java/kr/hs/emirim/w2036/chapter4_ex3/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2036.chapter4_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.solution4_2);
+    }
+}
